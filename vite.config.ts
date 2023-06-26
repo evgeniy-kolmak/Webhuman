646e9e950c45a4485a6a89025e2278480320da8b
evgeniy-kolmak <evgeniy.kolmak@gmail.com>
2023-06-26 20:35:27 +0300

    Added  svgrPlugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
+import svgrPlugin from 'vite-plugin-svgr';
 import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
   css: {
     postcss: {
       plugins: [autoprefixer],
