4c8ce8c0e40e4348ff7d48c39bf01141d2fe72d0
evgeniy-kolmak <evgeniy.kolmak@gmail.com>
2023-07-02 12:51:11 +0300

    Added smooth scroll & active class

--- src/components/HeaderGroup/Navigation/Navigation.tsx
+++ src/components/HeaderGroup/Navigation/Navigation.tsx
@@ -1,4 +1,5 @@
 import styles from './Navigation.module.sass';
+import { Link } from 'react-scroll';
 
 export type NavigationProps = {
   cnLinks?: string;
@@ -7,22 +8,30 @@ export type NavigationProps = {
 
 export const Navigation = ({ cnLinks, cnLink }: NavigationProps) => {
   const menuItems = [
-    { title: 'Главная' },
-    { title: 'Обо мне' },
-    { title: 'Опыт' },
-    { title: 'Портфолио' },
-    { title: 'Контакты' },
+    { title: 'Главная', link: 'main' },
+    { title: 'Обо мне', link: 'about' },
+    { title: 'Опыт', link: 'experience' },
+    { title: 'Портфолио', link: 'portfolio' },
+    { title: 'Контакты', link: 'contacts' },
   ];
 
-  const { navigation, link } = styles;
+  const { navigation, link, active } = styles;
 
   return (
     <ul className={[navigation, cnLinks].join(' ')}>
       {menuItems.map((item) => (
         <li className={cnLink} key={item.title}>
-          <a className={link} href="#">
+          <Link
+            className={link}
+            spy={true}
+            hashSpy={true}
+            smooth={true}
+            duration={500}
+            activeClass={active}
+            to={item.link}
+          >
             {item.title}
-          </a>
+          </Link>
         </li>
       ))}
     </ul>
